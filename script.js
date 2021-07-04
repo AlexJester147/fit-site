81ae2214e28d0d6ae383699aa5d70f53e5e267dc
AlexJester147 <vardy2016@lenta.ru>
2021-07-04 22:11:11 +0300

    feat : add overflow script for menu

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ setTimeout(()=>{
 burger.onclick = function(){
   menu.classList.toggle('active');
   opacityMenu.style.display = 'flex';
-
+  document.body.style.overflowY = 'hidden';
 };
 
 
 cross.onclick = function(){
   menu.classList.remove('active');
   opacityMenu.style.display = 'none';
-
+  document.body.style.overflowY = 'scroll';
 };
 
 
